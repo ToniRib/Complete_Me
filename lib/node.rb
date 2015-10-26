77afd945aa891d0b480f3d6b67a26347dbd37b50
ToniRib <tonimarierib@gmail.com>
2015-10-26 16:28:21 -0600

    feat: Node has attribute for word recognition
    ivar @valid_word is a boolean value that can be set to tell whether
    or not the word is a valid word. Will eventually be set when checked
    against dictionary.

--- lib/node.rb
+++ lib/node.rb
@@ -1,7 +1,14 @@
 class Node
   attr_reader :value
+  attr_accessor :valid_word, :links
 
   def initialize(value = '')
     @value = value
+    @valid_word = false
+    @links = {}
+  end
+
+  def word?
+    @valid_word
   end
 end
--- spec/node_test.rb
+++ spec/node_test.rb
@@ -7,13 +7,34 @@ class NodeTest < Minitest::Test
     assert Node
   end
 
+  def test_node_value_is_empty_string_by_default
+    node = Node.new
+
+    assert_equal '', node.value
+  end
+
   def test_node_has_a_value
     node = Node.new('p')
 
     assert_equal 'p', node.value
   end
 
-  def method_name
+  def test_node_is_not_a_word_by_default
+    node = Node.new('piz')
 
+    refute node.word?
+  end
+
+  def test_node_can_be_recognized_as_valid_word
+    node = Node.new('hello')
+    node.valid_word = true
+
+    assert node.word?
+  end
+
+  def test_node_has_a_links_hash_that_is_empty_by_default
+    node = Node.new('a')
+
+    assert_equal Hash.new, node.links
   end
 end
