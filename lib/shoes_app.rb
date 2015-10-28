4546f8d81ff6e15c60c5ca6e5b76707fa3ef9345
ToniRib <tonimarierib@gmail.com>
2015-10-27 22:25:50 -0600

    make app look nicer

--- lib/shoes_app.rb
+++ lib/shoes_app.rb
@@ -1,10 +1,13 @@
-Shoes.app do
+Shoes.app(width:350, height: 500) do
   background "#1fb2cc"
 
   stack(margin: 12) do
+    title "CompleteMe"
+
     para "Enter string to get suggestions:"
+
     flow do
-      @e = edit_line
+      @e = edit_line width: 100
       @push = button "search"
     end
     @s = para 'no suggestions yet...'
