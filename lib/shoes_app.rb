229151c3d5922b1c6ec5b5ba354603931d40d921
ToniRib <tonimarierib@gmail.com>
2015-10-28 15:29:37 -0600

    feat: Update Shoes App for include search

--- lib/shoes_app.rb
+++ lib/shoes_app.rb
@@ -2,7 +2,7 @@ Shoes.setup do
   require './complete_me'
 end
 
-Shoes.app(width:350, height: 500) do
+Shoes.app(width:500, height: 500) do
   background "#1fb2cc"
 
   stack(margin: 12) do
@@ -12,12 +12,19 @@ Shoes.app(width:350, height: 500) do
 
     flow do
       @e = edit_line width: 100
-      @push = button "search"
+      @push_start = button "search (starts with)"
+      @push_all = button "search (includes)"
     end
+
     @s = para 'no suggestions yet...'
 
-    @push.click {
+    @push_start.click {
       @matches = @completion.suggest(@e.text)
+      @s.replace @matches.join("\n")
+    }
+
+    @push_all.click {
+      @matches = @completion.suggest_substring(@e.text)
       @s.replace @matches.join("\n")
     }
   end
